e6bec48954b0d08d7df1cec28b32720443e6b10d
Konstantin <rediskadota@gmail.com>
2017-12-06 17:45:50 +0200

    test1

---
+++ TestPageUI/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestPageUI
+{
+    [TestFixture(Platform.Android)]
+   // [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp.Android.
+                ApkFile(@"C:\Users\sunrise\Desktop\com.companyname.Phoneword-Signed.apk").
+                StartApp();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+           // app.WaitForElement(c => c.Marked("MyButton"));
+            //app.Screenshot("asd");
+            app.Tap(c => c.Text("Change page"));
+        }
+    }
+}
+
